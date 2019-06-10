0ef9f1c8336db4fbde4eb6dadda4372616b13a99
Cokde Susila <cokdes@gmail.com>
2019-06-11 02:41:31 +0800

    end of section 6

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,20 +1,39 @@
-import React, { Component, Fragment } from 'react';
-import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
-import NavBar from '../../features/nav/NavBar/NavBar';
-import { Container } from 'semantic-ui-react';
+import React, { Component, Fragment } from "react";
+import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
+import NavBar from "../../features/nav/NavBar/NavBar";
+import { Container } from "semantic-ui-react";
+import { Route } from "react-router";
+import HomePage from "../../features/home/HomePage";
+import EventForm from "../../features/event/EventForm/EventForm";
+import PeopleDashboard from "../../features/user/PeopleDashboard/PeopleDashboard";
+import EventDetailedPage from "../../features/event/EventDetailed/EventDetailedPage";
+import UserDetailedPage from "../../features/user/UserDetailed/UserDetailedPage";
+import SettingDashboard from "../../features/user/Settings/SettingDashboard";
 
 class App extends Component {
-  render(){
+  render() {
     return (
       <Fragment>
-        <NavBar/>
-        <Container className="main">
-          <EventDashboard/>
-        </Container>
+        <Route exact path="/" component={HomePage} />
+        <Route
+          path="/(.+)"
+          render={() => (
+            <Fragment>
+              <NavBar />
+              <Container className="main">
+                <Route path="/events" component={EventDashboard} />
+                <Route path="/events/:id" component={EventDetailedPage} />
+                <Route path="/people" component={PeopleDashboard} />
+                <Route path="/profile/:id" component={UserDetailedPage} />
+                <Route path="/settings" component={SettingDashboard} />
+                <Route path="/createEvents" component={EventForm} />
+              </Container>
+            </Fragment>
+          )}
+        />
       </Fragment>
     );
   }
 }
 
 export default App;
-
--- src/features/user/Settings/SettingNav.jsx
+++ src/features/user/Settings/SettingNav.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { Fragment } from "react";
+import { Menu, Header } from "semantic-ui-react";
+import { NavLink } from "react-router-dom";
 
 const SettingNav = () => {
-    return (
-        <div>
-            <h1>Nav for User</h1>
-        </div>
-    )
-}
+  return (
+    <Fragment>
+      <Menu vertical>
+        <Header icon="user" attached inverted color="grey" content="Profile" />
+        <Menu.Item as={NavLink} to='/settings/basic'>Basics</Menu.Item>
+        <Menu.Item as={NavLink} to='/settings/about'>About Me</Menu.Item>
+        <Menu.Item as={NavLink} to='/settings/photos'>My Photos</Menu.Item>
+      </Menu>
 
-export default SettingNav
+      <Menu vertical>
+        <Header
+          icon="settings"
+          attached
+          inverted
+          color="grey"
+          content="Account"
+        />
+        <Menu.Item as={NavLink} to='/settings/account'>My Account</Menu.Item>
+      </Menu>
+    </Fragment>
+  );
+};
+
+export default SettingNav;
