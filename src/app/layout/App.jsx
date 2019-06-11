fb26a5b6511ec764437d8eb4b6bb9b7a2f43e2a3
Cokde Susila <cokdes@gmail.com>
2019-06-12 07:36:39 +0800

    add redux to create event

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from "react";
 import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
 import NavBar from "../../features/nav/NavBar/NavBar";
 import { Container } from "semantic-ui-react";
-import { Route } from "react-router";
+import { Route, Switch, withRouter } from "react-router";
 import HomePage from "../../features/home/HomePage";
 import EventForm from "../../features/event/EventForm/EventForm";
 import PeopleDashboard from "../../features/user/PeopleDashboard/PeopleDashboard";
@@ -22,16 +22,18 @@ class App extends Component {
             <Fragment>
               <NavBar />
               <Container className="main">
-                <Route exact path="/events" component={EventDashboard} />
-                <Route path="/events/:id" component={EventDetailedPage} />
-                <Route path="/people" component={PeopleDashboard} />
-                <Route path="/profile/:id" component={UserDetailedPage} />
-                <Route path="/settings" component={SettingDashboard} />
-                <Route
-                  path={["/createEvents", "/manage/:id"]}
-                  component={EventForm}
-                />
-                <Route path="/test" component={TestComponent} />
+                <Switch key={this.props.location.key}>
+                  <Route exact path="/events" component={EventDashboard} />
+                  <Route path="/events/:id" component={EventDetailedPage} />
+                  <Route path="/people" component={PeopleDashboard} />
+                  <Route path="/profile/:id" component={UserDetailedPage} />
+                  <Route path="/settings" component={SettingDashboard} />
+                  <Route
+                    path={["/createEvents", "/manage/:id"]}
+                    component={EventForm}
+                  />
+                  <Route path="/test" component={TestComponent} />
+                </Switch>
               </Container>
             </Fragment>
           )}
@@ -41,4 +43,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withRouter(App);
--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
-import { Grid, Button } from "semantic-ui-react";
+import { Grid } from "semantic-ui-react";
 import { connect } from "react-redux";
 import EventList from "../EventList/EventList";
-import EventForm from "../EventForm/EventForm";
 import cuid from "cuid";
-import {createEvent, deleteEvent, updateEvent} from '../eventActions'
+import { createEvent, deleteEvent, updateEvent } from "../eventActions";
 
 const mapState = state => ({
   events: state.events
@@ -14,92 +13,42 @@ const actions = {
   createEvent,
   deleteEvent,
   updateEvent
-}
+};
 
 class EventDashboard extends Component {
-  state = {
-    isOpen: false,
-    selectedEvent: null
-  };
-
-  // handleIsOpenToggle = () => {
-  //   this.setState(({isOpen}) => ({
-  //     isOpen: !isOpen
-  //   }))
-  // }
-
-  handleCreateFormOpen = () => {
-    this.setState({
-      isOpen: true,
-      selectedEvent: null
-    });
-  };
-
-  handleFormCancel = () => {
-    this.setState({
-      isOpen: false
-    });
-  };
-
   handleCreateEvent = newEvent => {
     newEvent.id = cuid();
     newEvent.hostPhotoURL = "/assets/user.png";
     this.props.createEvent(newEvent);
-    this.setState(({ events }) => ({
-      isOpen:false
-    }));
-  };
-
-  handleSelectEvent = event => {
-    this.setState({
-      selectedEvent: event,
-      isOpen: true
-    });
   };
 
   handleUpdateEvent = updatedEvent => {
-    this.props.updateEvent(updatedEvent)
-    this.setState(({ events }) => ({
-      isOpen: false,
-      selectedEvent: null
-    }));
+    this.props.updateEvent(updatedEvent);
   };
 
   handleDeleteEvent = id => {
-    this.props.deleteEvent(id)
+    this.props.deleteEvent(id);
   };
 
   render() {
-    const { isOpen, selectedEvent } = this.state;
     const { events } = this.props;
     return (
       <Grid>
         <Grid.Column width={10}>
           <EventList
             events={events}
-            selectEvent={this.handleSelectEvent}
             deleteEvent={this.handleDeleteEvent}
           />
         </Grid.Column>
         <Grid.Column width={6}>
-          <Button
-            onClick={this.handleCreateFormOpen}
-            positive
-            content="Create Event"
-          />
-          {isOpen && (
-            <EventForm
-              key={selectedEvent ? selectedEvent.id : 0}
-              updateEvent={this.handleUpdateEvent}
-              selectedEvent={selectedEvent}
-              createEvent={this.handleCreateEvent}
-              cancelFormOpen={this.handleFormCancel}
-            />
-          )}
+          <h2>Activity Feed</h2>
         </Grid.Column>
       </Grid>
     );
   }
 }
 
-export default connect(mapState, actions)(EventDashboard);
+export default connect(
+  mapState,
+  actions
+)(EventDashboard);
